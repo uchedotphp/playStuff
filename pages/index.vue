cb44cdffc800fb3b8a798d0ec62ac3a855579083
Nwulu Uchechukwu Prince <uchedotphp@gmail.com>
2021-10-17 12:01:08 +0100

    step navigation

--- pages/index.vue
+++ pages/index.vue
@@ -1,19 +1,24 @@
 <template>
   <div class="row min-height">
     <div
-      class="left col-4 full-height orange-backdrop d-grid place-content-center"
+      class="left col-4 full-height orange-backdrop d-grid place-content-end p-b-50"
     >
       <span>
         <img src="~/assets/images/image1.png" alt="" />
       </span>
     </div>
     <div class="right col-8 full-height">
-      <div class="container text-right">
+      <div class="container">
         <div class="d-flex justify-end">
-        <button class="button button-fill-red">
-          <p class="p-r-5 p-l-5 p-t-2 p-b-2">Logout</p>
-        </button>
+          <button class="button button-fill-red">
+            <p class="p-r-5 p-l-5 p-t-2 p-b-2">Logout</p>
+          </button>
         </div>
+
+        <!-- step navigation -->
+        <step-navigation class="m-t-20" />
+
+        <hr class="m-t-15" />
       </div>
     </div>
   </div>
