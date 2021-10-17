1a1787e244baff08b3cf3af7ad6201c37a3074fc
Nwulu Uchechukwu Prince <uchedotphp@gmail.com>
2021-10-17 10:23:21 +0100

    bas scaffold

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,32 @@
 <template>
-  <Tutorial/>
+  <div class="row min-height">
+    <div
+      class="left col-4 full-height orange-backdrop d-grid place-content-center"
+    >
+      <span>
+        <img src="~/assets/images/image1.png" alt="" />
+      </span>
+    </div>
+    <div class="right col-8 full-height">
+      <div class="container text-right">
+        <div class="d-flex justify-end">
+        <button class="button button-fill-red">
+          <p class="p-r-5 p-l-5 p-t-2 p-b-2">Logout</p>
+        </button>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
 export default {}
 </script>
+<style scoped>
+/* Extra small devices (phones, 600px and down) */
+@media only screen and (max-width: 42em) {
+  .left {
+    display: none;
+  }
+}
+</style>
